6de649d2101f692acab7479d7ea222da102da26c
Joe W <jweber4213@gmail.com>
2023-03-07 07:39:28 +0900

    first draft

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # LandingPage
 LandingPage Trial Design
+
+
+
+
+Credits: 
+
+Tomáš Malík for the background Image (Red and Gray Pagoda Temple, Shizuoka) - Pexels
+
+Freepik / Flaticon for the mountain icon
---
+++ script.js
@@ -0,0 +1,6 @@
+const gear = [
+    {type: 'Hiking Backpack', weight: 'Medium', cost: '¥12,000', pros: 'Carries everything, frame to support your back'},
+    {type: 'Hiking Boots', weight: '907g', cost: '¥12,000', pros: 'Supports your feet, great for rough terrain'},
+    {type: '', weight: '', cost: '', pros: ''},
+    {type: '', weight: '', cost: '', pros: ''},
+]
